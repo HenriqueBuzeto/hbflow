bbe133f84b859d5931b52f2d7d34633b7d85ae07
Henrique Buzeto <henriquebv.vieirads@gmail.com>
2026-06-12 10:15:59 -0300

    ajuste de logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -106,7 +106,7 @@ export default function Sidebar() {
       <div className="h-16 flex items-center justify-center border-b border-zinc-800 shrink-0 relative px-4">
         {!isCollapsed && (
           <div className="flex items-center justify-center h-full">
-            <img src="/logo hbflow.png" alt="HBFlow Logo" className="h-[450px] w-auto object-contain my-[-200px] ml-[-125px] mr-[-205px]" />
+            <img src="/logo hbflow.png" alt="HBFlow Logo" className="h-[450px] w-auto object-contain my-[-200px] ml-[-145px] mr-[-185px]" />
           </div>
         )}
         
