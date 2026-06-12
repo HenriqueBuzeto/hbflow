7ddd08bd0bc4c85d1fcff695d06d7299761a3a1d
Henrique Buzeto <henriquebv.vieirads@gmail.com>
2026-06-12 08:35:47 -0300

    chore: configurar legacy-peer-deps e instalar dependencias

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {},
 };
 
 export default withSentryConfig(nextConfig, {
